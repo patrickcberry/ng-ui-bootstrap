adb59f7a89cf477db31d312c66fd71c8689cb74b
patrickcberry <patrickcberry@gmail.com>
2018-05-23 07:06:07 +1000

    Initial development

---
+++ src/app/side-bar/sb/sb.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sb',
+  templateUrl: './sb.component.html',
+  styleUrls: ['./sb.component.scss']
+})
+export class SbComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/side-bar/side-bar.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SbComponent } from './sb/sb.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [SbComponent]
+})
+export class SideBarModule { }
